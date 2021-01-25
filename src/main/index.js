2e62d49ebf1b710aeb7fe7d6a0820ed6e18fb7bd
Alexandr Romanov <romanov.spb.alex@yandex.ru>
2021-01-25 13:37:34 +0300

    updated

--- src/main/index.js
+++ src/main/index.js
@@ -1,5 +1,6 @@
 import React, {useState} from 'react';
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
+import AnchorLink from 'react-anchor-link-smooth-scroll';
 
 import GlobalFonts from './GlobalFonts';
 import farm from './farm.svg';
@@ -33,6 +34,14 @@ const Wrap = styled.div`
   max-width: 100vw;
   background: linear-gradient(#E5E5E5, #FFFFFF);
   position: relative;
+
+  & img {
+    -webkit-user-drag: none;
+    -khtml-user-drag: none;
+    -moz-user-drag: none;
+    -o-user-drag: none;
+    user-drag: none;
+  }
 `;
 
 const Ellipse1 = styled.img`
@@ -279,7 +288,7 @@ const ImageBlock = styled.div`
 `;
 
 const FormBlock = styled.div`
-  z-index: 999;
+  z-index: 100;
   @media ${device.mobileS} {
     width: 90vw;
     margin-left: 5vw;
@@ -294,10 +303,11 @@ const FormBlock = styled.div`
   align-items: center;
   padding-top: 150px;
   font-family: sans-serif;
+  position: relative;
 `;
 
 const ConsentRow = styled.div`
-  z-index: 999;
+  z-index: 100;
   display: flex;
   flex-direction: row;
   align-items: center;
@@ -309,7 +319,7 @@ const ConsentRow = styled.div`
 `;
 
 const Input = styled.input`
-  z-index: 999;
+  z-index: 100;
   @media ${device.mobileS} {
     width: 90vw;
   }
@@ -332,7 +342,7 @@ const Input = styled.input`
 `;
 
 const Checkbox = styled.input`
-  z-index: 999;
+  z-index: 100;
   width: 50px;
   height: 50px;
   margin-left: 40px;
@@ -360,6 +370,30 @@ const Footer = styled.div`
   }
 `;
 
+const Modal = styled.div`
+  @media ${device.mobileS} {
+    width: 90vw;
+    height: 25vh;
+  }
+  @media ${device.laptop} {
+    width: 40vw;
+    height: 15vh;
+  }
+  background: white;
+  box-shadow: 1px 0px 5px rgba(0,0,0,0.3);
+  position: absolute;
+  z-index: 999;
+  border-radius: 10px;
+  display: flex;
+  ${props => props.hidden && css`
+    display: none;
+  `}
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+`;
+
 const Index = () => {
   const [name, setName] = useState('');
   const [occupation, setOccupation] = useState('');
@@ -367,18 +401,19 @@ const Index = () => {
   const [phone, setPhone] = useState('');
   const [email, setEmail] = useState('');
   const [aggreed, setAggreed] = useState(false);
+  const [unsent, setUnsent] = useState(true);
 
   const fetchEmail = () => {
     if(!aggreed) return;
     var myHeaders = new Headers();
-    myHeaders.append("Content-Type", "application/x-www-form-urlencoded");
+    myHeaders.append('Content-Type', 'application/x-www-form-urlencoded');
 
     var urlencoded = new URLSearchParams();
-    urlencoded.append("name", name);
-    urlencoded.append("phone", phone);
-    urlencoded.append("email", email);
-    urlencoded.append("occupation", occupation);
-    urlencoded.append("company", company);
+    urlencoded.append('name', name);
+    urlencoded.append('phone', phone);
+    urlencoded.append('email', email);
+    urlencoded.append('occupation', occupation);
+    urlencoded.append('company', company);
 
     var requestOptions = {
       method: 'POST',
@@ -387,9 +422,14 @@ const Index = () => {
       redirect: 'follow'
     };
 
-    fetch("localhost:8080/", requestOptions)
+    fetch('176.119.156.180:8080/', requestOptions)
       .then(response => response.text())
-      .then(result => console.log(result))
+      .then(result => {
+        setUnsent(false);
+        setTimeout(() => {
+          setUnsent(true);
+        }, 3000);
+      })
       .catch(error => console.log('error', error));
   }
 
@@ -412,34 +452,43 @@ const Index = () => {
         <h1>АО «ВетроОГК-2» (340 МВт) и ее место в ESG-стратегиях ГК «Росатом» и Банка ГПБ (АО)</h1>
         <h4>27 января 2021 года, 10:00</h4>
         <ButtonsRow>
-          <a href={process.env.PUBLIC_URL + '/prgrm.pdf'} rel="noreferrer" target='_blank'>
+          <a href={process.env.PUBLIC_URL + '/prgrm.pdf'} rel='noreferrer' target='_blank'>
             <Button>Программа</Button>
           </a>
-          <Button>Регистрация</Button>
+          <AnchorLink href='#reg'>
+            <Button>Регистрация</Button>
+          </AnchorLink>
         </ButtonsRow>
       </WelcomeBlock>
       <ImageBlock>
         <img src={farm} alt='farm' />
       </ImageBlock>
-      <FormBlock>
-        <Input value={name} onChange={(e) => setName(e.target.value)} placeholder='Фамилия Имя Отчество' />
-        <Input value={occupation} onChange={(e) => setOccupation(e.target.value)} placeholder='Должность' />
-        <Input value={phone} onChange={(e) => setPhone(e.target.value)} placeholder='Телефон' />
-        <Input value={email} onChange={(e) => setEmail(e.target.value)} placeholder='E-mail' />
-        <Input value={company} onChange={(e) => setCompany(e.target.value)} placeholder='Компания' />
-        <ConsentRow>
-          <Checkbox type='checkbox' id='consent' name='consent' value='Aggree' onClick={() => { setAggreed(!aggreed) }} />
-          <p>Я принимаю Условия Пользовательского соглашения и даю свое согласие на обработку моей персональной информации на условиях определенных <a href={process.env.PUBLIC_URL + '/policy.pdf'} rel="noreferrer" target='_blank'>Политикой конфиденциальности</a></p>
-        </ConsentRow>
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <ButtonLarge onClick={fetchEmail}>Зарегистрироваться</ButtonLarge>
-      </FormBlock>
+      <section id='reg'>
+        <FormBlock>
+          <Modal hidden={unsent}>
+            <h3>
+              Ваша регистрационная форма получена.<br />Мы свяжемся с Вами в ближайшее время.
+            </h3>
+          </Modal>
+          <Input value={name} onChange={(e) => setName(e.target.value)} placeholder='Фамилия Имя Отчество' />
+          <Input value={occupation} onChange={(e) => setOccupation(e.target.value)} placeholder='Должность' />
+          <Input value={phone} onChange={(e) => setPhone(e.target.value)} placeholder='Телефон' />
+          <Input value={email} onChange={(e) => setEmail(e.target.value)} placeholder='E-mail' />
+          <Input value={company} onChange={(e) => setCompany(e.target.value)} placeholder='Компания' />
+          <ConsentRow>
+            <Checkbox type='checkbox' id='consent' name='consent' value='Aggree' onClick={() => { setAggreed(!aggreed) }} />
+            <p>Я принимаю Условия Пользовательского соглашения и даю свое согласие на обработку моей персональной информации на условиях определенных <a href={process.env.PUBLIC_URL + '/policy.pdf'} rel='noreferrer' target='_blank'>Политикой конфиденциальности</a></p>
+          </ConsentRow>
+          <br />
+          <br />
+          <br />
+          <br />
+          <br />
+          <br />
+          <br />
+          <ButtonLarge onClick={fetchEmail}>Зарегистрироваться</ButtonLarge>
+        </FormBlock>
+      </section>
       <Footer>
         <img src={partner1alt} alt='gazprom' />
       </Footer>
